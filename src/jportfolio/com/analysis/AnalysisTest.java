7d31360d7eeaf768acdc68a5a40814876d99a8d9
wendelfariaslopes <wendellopesfarias@gmail.com>
2017-10-22 20:49:02 -0400

    :memo: Test failed to more 100,000,000

--- src/jportfolio/com/analysis/AnalysisTest.java
+++ src/jportfolio/com/analysis/AnalysisTest.java
@@ -1,6 +1,7 @@
 package jportfolio.com.analysis;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -10,9 +11,7 @@ public class AnalysisTest {
 
 	public static void main(String[] args) {
 
-		
 		Analysis a = new Analysis();
-		
 
 		double[] pA = {33.12, 34.89, 34.35, 34.29, 34.02, 34.64, 33.97, 34.08, 34.30, 33.26, 30.28, 31.60};
 		double[] pB = {38.98, 39.21, 33.96, 33.42, 31.54, 29.40, 30.67, 27.19, 25.49, 25.96, 26.01, 22.99};
@@ -21,18 +20,31 @@ public class AnalysisTest {
 		double[] pE = {38.18, 39.11, 33.16, 33.12, 31.14, 29.40, 30.67, 37.19, 35.49, 37.96, 36.01, 32.99};
 		double[] pF = {38.18, 39.11, 33.16, 33.12, 31.14, 29.40, 30.67, 37.19, 35.49, 37.96, 36.01, 32.99};
 		
-		List<Portfolio> list = a.generateCombinationPortfolios(100,pA,pB);
+		List<Portfolio> list = a.generateCombinationPortfolios(1000000,pA,pB);
 		
-		Portfolio pMinRisk = list
-			      .stream()
-			      .min(Comparator.comparing(Portfolio::getRiskValue))
-			      .orElseThrow(NoSuchElementException::new);
+		List<Portfolio> listTop5 = new ArrayList<Portfolio>();
 		
-		for (Portfolio p: list) {
+		
+		for(int i=0; i< 20; i++){
+			Portfolio pMinRisk = list
+				      .stream()
+				      .min(Comparator.comparing(Portfolio::getRiskValue))
+				      .orElseThrow(NoSuchElementException::new);
+			listTop5.add(pMinRisk);
+			list.remove(pMinRisk);
+		}
+		
+//		Portfolio pMinRisk = list
+//			      .stream()
+//			      .min(Comparator.comparing(Portfolio::getRiskValue))
+//			      .orElseThrow(NoSuchElementException::new);
+		
+		
+		for (Portfolio p: listTop5) {
 			System.out.print(p);
 		}
 		
-		System.out.println("RiskMin = "+pMinRisk);
+		//System.out.println("RiskMin = "+pMinRisk);
 		
 
 	}
