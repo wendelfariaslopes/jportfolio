65ca84143ccc20da69095bc30b4d93ef593ae71c
wendelfariaslopes <wendellopesfarias@gmail.com>
2019-04-27 10:48:54 -0400

    Stocks

---
+++ src/jportfolio/com/trade/Arbitrage.java
@@ -0,0 +1,21 @@
+package jportfolio.com.trade;
+
+/**
+ * Arbitrage is the practice of taking
+ * advantage of a price difference between two or more markets: striking a
+ * combination of matching deals that capitalize upon the imbalance, the profit
+ * being the difference between the market prices.
+ * 
+ * Link: <a href="" target="_blank">Arbitrage wikipedia</a>
+ * 
+ * @author wendellopes
+ *
+ */
+public class Arbitrage {
+
+	public static void main(String[] args) {
+		// Price
+
+	}
+
+}
